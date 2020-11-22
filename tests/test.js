6417909987f9df4425daa761b2c9e077429ff5e5
shaannessy25 <shaan.hurley25@gmail.com>
2020-11-21 17:41:02 -0800

    adding test for hours, mins, and secs

--- tests/test.js
+++ tests/test.js
@@ -23,3 +23,39 @@ test("Testing Get Month", () => {
 
 })
 
+test("Testing get Day", () => {
+    const d = new dateLib.D(2020, 0, 1)
+    const d2 = new dateLib.D(2019, 3, 25)
+    const d3 = new dateLib.D(2020, 4, 3)
+    expect(d.day).toBe(1)
+    expect(d2.day).toBe(25)
+    expect(d3.day).toBe(3)
+})
+
+test("Testing get hour", () => {
+    const d = new dateLib.D(2017, 0, 2, 3, 4, 5)
+    const d2 = new dateLib.D(2019, 0, 2, 7, 4, 5)
+    const d3 = new dateLib.D(2020, 0, 2, 12, 4, 5)
+    expect(d.hour).toBe(3)
+    expect(d2.hour).toBe(7)
+    expect(d3.hour).toBe(12)
+})
+
+
+test("Testing get minutes", () => {
+    const d = new dateLib.D(2017, 0, 2, 3, 4, 5)
+    const d2 = new dateLib.D(2019, 0, 2, 7, 45, 5)
+    const d3 = new dateLib.D(2020, 0, 2, 12, 30, 5)
+    expect(d.min).toBe(4)
+    expect(d2.min).toBe(45)
+    expect(d3.min).toBe(30)
+})
+
+test("Testing get secs", () => {
+    const d = new dateLib.D(2017, 0, 2, 3, 4, 45)
+    const d2 = new dateLib.D(2019, 0, 2, 7, 4, 25)
+    const d3 = new dateLib.D(2020, 0, 2, 12, 4, 15)
+    expect(d.secs).toBe(45)
+    expect(d2.secs).toBe(25)
+    expect(d3.secs).toBe(15)
+})
